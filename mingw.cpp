56314c9ae052a86b107e900cf305612b7edc204e
SteakOverCooked.com <oh@steakovercooked.com>
2017-02-03 13:43:10 +0000

    mingw.cpp
    gcc (MingGW) x86 OpenMP

---
+++ mingw.cpp
@@ -0,0 +1,58 @@
+#include <math.h>
+#include <iostream>
+#include <windows.h>
+#include <omp.h>
+
+using namespace std;
+
+const int MAXN = 10000000;
+
+int IsPrime(int number)
+{
+	if (number <= 1) return 0;
+	int right = (int)floor(sqrt(number));
+	for (int index = 2; index <= right; ++index)
+		if (number % index == 0)
+			return 0;
+	return 1;
+}
+
+int Single(int n) {
+	int s = 0;
+	for (int i = 1; i <= n; ++i) {
+		if (IsPrime(i)) {
+			++s;
+		}
+	}
+	return s;
+}
+
+int Parallel1(int n) {
+	volatile long s = 0;
+
+	#pragma omp parallel for
+	for (int i = 1; i <= n; ++i) {
+		if (IsPrime(i)) {
+			InterlockedIncrement(&s);
+		}
+	}
+	return s;
+}
+
+int main() {
+    cout << sizeof(void*) << endl;
+	DWORD serial, parallel1, parallel2, s;
+	DWORD startTime = GetTickCount();
+	s = Single(MAXN);
+	cout << s << endl;
+	serial = GetTickCount() - startTime;
+
+	startTime = GetTickCount();
+	s = Parallel1(MAXN);
+	parallel1 = GetTickCount() - startTime;
+	cout << s << endl;
+
+	cout << "Serial = " << serial << endl;
+	cout << "Parallel1 = " << parallel1 << endl;
+	return 0;
+}
