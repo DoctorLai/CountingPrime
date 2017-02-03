9aad16faf4953ae926abed8ea7409a786ce3ccde
SteakOverCooked.com <oh@steakovercooked.com>
2017-02-03 13:41:37 +0000

    vs.cpp
    Visual Studio 2015  C++ x86, x64,  Console Application

---
+++ vs.cpp
@@ -0,0 +1,56 @@
+#include "stdafx.h"
+#include <iostream>
+#include <windows.h>
+#include <math.h>
+#include <ppl.h>
+
+using namespace std;
+
+const int MAXN = 10000000;
+
+int IsPrime(int number)
+{
+	if (number <= 1) return 0;
+	int right = (int)floor(sqrt(number));
+	for (int index = 2; index <= right; ++index)
+		if (number % index == 0)
+			return 0;
+	return 1;
+}
+
+int Single(int n) {
+	int s = 0;
+	for (int i = 1; i <= n; ++i) {
+		if (IsPrime(i)) {
+			++s;
+		}
+	}
+	return s;
+}
+
+int Parallel1(int n) {
+	volatile long s = 0;
+	Concurrency::parallel_for(0, n, [&](int i) {
+		if (IsPrime(i)) {
+			InterlockedIncrement(&s);
+		}
+	});
+	return s;
+}
+
+int main() {
+	DWORD serial, parallel1, s;
+	DWORD startTime = GetTickCount();
+	s = Single(MAXN);
+	cout << s << endl;
+	serial = GetTickCount() - startTime;
+
+	startTime = GetTickCount();
+	s = Parallel1(MAXN);
+	parallel1 = GetTickCount() - startTime;
+	cout << s << endl;
+
+	cout << "Serial = " << serial << endl;
+	cout << "Parallel1 = " << parallel1 << endl;
+	return 0;
+}
